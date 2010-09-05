040707706817ccee10c8973a088a195bde21f91a
Will Luongo <me@willluongo.com>
2010-09-05 00:01:43 -0400

    Removed debugging code from animation function. 	modified:   Smuggler 	modified:   animate.cpp

--- animate.cpp
+++ animate.cpp
@@ -9,33 +9,27 @@ int animate(int xvel, int yvel, int movementspeed, int walkFrames, int walkFrame
 	if (walkFrames < walkFrameInterval)
 	{
 		object.frameY(200);
-		printf("frameY200");
 	}
 	
 	if ((walkFrames < walkFrameInterval*2) && (walkFrames > walkFrameInterval))
 	{
 		object.frameY(400);
-		printf("frameY400");
 	}
 	
 	if (yvel == movementspeed)
 	{
-		printf("frameX0");
 		object.frameX(0);
 	}
 	if (yvel == -movementspeed)
 	{
-		printf("frameX200");
 		object.frameX(200);
 	}
 	if (xvel == movementspeed)
 	{
-		printf("frameX400");
 		object.frameX(400);
 	}
 	if (xvel == -movementspeed)
 	{
-		printf("frameX600");
 		object.frameX(600);
 	}
 	if (walkFrames == (walkFrameInterval*2))
@@ -45,10 +39,8 @@ int animate(int xvel, int yvel, int movementspeed, int walkFrames, int walkFrame
 	if ((xvel==0) && (yvel == 0))
 	{
 		object.frameY(0);
-		walkFrames = 0;
 	}
 	
-	printf("walkFrames: %d\n", walkFrames);
 	walkFrames +=1;
 	return walkFrames;
 
